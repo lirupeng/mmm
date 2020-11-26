b50ab1ac5e358bbbabf979396d2a03960a2d1c2f
lirupeng <15802222869@126.com>
2020-11-26 15:38:13 +0800

    目录搭建

---
+++ App.vue
@@ -0,0 +1,21 @@
+<template>
+</template>
+<script>
+  import {mapGetters,mapActions} from 'vuex';
+export default {
+props: [],
+components: {},
+data() {
+return {};
+},
+computed: {
+...mapGetters({}),
+},
+methods: {
+...mapActions({}),
+},
+mounted() {},
+};
+</script>
+<style scoped>
+</style>
---
+++ components/menu/menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    thi is menu
+  </div>
+</template>
+<script>
+  import {mapGetters,mapActions} from 'vuex';
+export default {
+props: [],
+components: {},
+data() {
+return {};
+},
+computed: {
+...mapGetters({}),
+},
+methods: {
+...mapActions({}),
+},
+mounted() {},
+};
+</script>
+<style scoped>
+</style>
